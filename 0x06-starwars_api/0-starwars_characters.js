6d07a1748eab73dc6ec3373a5f89ca0dd9138ade
ZaneleYenziwe <zaneleyenziwe19@gmail.com>
2024-11-13 20:12:10 +0000

    star wars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,31 @@
+#!/usr/bin/node
+
 const request = require('request');
 const movieId = process.argv[2];
 
 if (!movieId) {
-    console.log('Usage: node 0-starwars_characters.js <movie_id>');
-    process.exit(1);
+  console.log('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
 }
 
 const url = `https://swapi.dev/api/films/${movieId}/`;
 
 request(url, { json: true }, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-        console.log('Failed to retrieve data');
-        return;
-    }
+  if (err || res.statusCode !== 200) {
+    console.log('Failed to retrieve data');
+    return;
+  }
 
-    const characters = body.characters;
-    characters.forEach(characterUrl => {
-        request(characterUrl, { json: true }, (err, res, characterBody) => {
-            if (err || res.statusCode !== 200) {
-                console.log('Failed to retrieve character data');
-                return;
-            }
-            console.log(characterBody.name);
-        });
+  const characters = body.characters;
+
+  characters.forEach((characterUrl) => {
+    request(characterUrl, { json: true }, (err, res, characterBody) => {
+      if (err || res.statusCode !== 200) {
+        console.log('Failed to retrieve character data');
+        return;
+      }
+      console.log(characterBody.name);
     });
+  });
 });
 
